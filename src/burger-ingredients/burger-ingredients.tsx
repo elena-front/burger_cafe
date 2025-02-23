06d6d6e33354693871f1f94ce0e197efd179b88d
elena-front <shumkova.helen@yandex.ru>
2025-02-23 17:44:45 +0400

    count added

--- src/burger-ingredients/burger-ingredients.tsx
+++ src/burger-ingredients/burger-ingredients.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
 	Tab,
 	CurrencyIcon,
+	Counter,
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import './burger-ingredients.css';
 import {
@@ -10,22 +11,34 @@ import {
 	itemsSause,
 } from './burger-ingredients-data';
 import { Ingredient } from '../types';
+import { myBurger } from '../burger-constructor/burger-constructor-data';
 
 type ListProps = {
 	items: Ingredient[];
+	selectedItems: Ingredient[];
 };
 
-function List({ items }: ListProps) {
-	const listItems = items.map((item, index) => (
-		<li className='card'>
-			<img src={item.img} className='cardImg'></img>
-			<p className='text text_type_main-small price'>
-				<div>{item.price.toString()}</div>
-				<CurrencyIcon type='primary' />
-			</p>
-			<p className='text text_type_main-small'>{item.name}</p>
-		</li>
-	));
+function List({ items, selectedItems }: ListProps) {
+	const listItems = items.map((item, index) => {
+		const count = selectedItems.filter(
+			(selectedItem) => item.name === selectedItem.name
+		).length;
+
+		return (
+			<li key={index} className='card'>
+				{count > 0 && (
+					<Counter count={count} size='default' extraClass='m-1 counter' />
+				)}
+
+				<img src={item.img} className='cardImg'></img>
+				<p className='text text_type_main-small price'>
+					<div>{item.price.toString()}</div>
+					<CurrencyIcon type='primary' />
+				</p>
+				<p className='text text_type_main-small'>{item.name}</p>
+			</li>
+		);
+	});
 	return <ul className='cardsList'>{listItems}</ul>;
 }
 
@@ -54,15 +67,15 @@ export const BurgerIngredients = () => {
 			<div className='mainMenu custom-scroll'>
 				<div className='chapter mt-10'>
 					<h2 className='text text_type_main-medium'>Булки</h2>
-					<List items={itemsBread}></List>
+					<List items={itemsBread} selectedItems={myBurger}></List>
 				</div>
 				<div className='chapter mt-10'>
 					<h2 className='text text_type_main-medium'>Соусы</h2>
-					<List items={itemsSause}></List>
+					<List items={itemsSause} selectedItems={myBurger}></List>
 				</div>
 				<div className='chapter mt-10'>
 					<h2 className='text text_type_main-medium'>Начинки</h2>
-					<List items={itemsFilling}></List>
+					<List items={itemsFilling} selectedItems={myBurger}></List>
 				</div>
 			</div>
 		</div>
