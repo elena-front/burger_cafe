15547dca15b815849615e1ebfd9e09a0bb0ae602
elena-front <shumkova.helen@yandex.ru>
2025-03-04 22:56:15 +0400

    Added redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/app/app';
 import './styles.css';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './services/reducers';
+
+const composeEnhancers =
+	typeof window === 'object' &&
+	(window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose;
+
+const enhancer = composeEnhancers();
+
+const store = createStore(rootReducer, enhancer);
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
