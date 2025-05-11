05603df56b422e561620172cd4e2f55d153f1a22
elena-front <shumkova.helen@yandex.ru>
2025-05-11 23:28:17 +0400

    add gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import App from './components/app/app';
 import './styles.css';
 import { Provider } from 'react-redux';
 import { store } from './services/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	</StrictMode>
 );
